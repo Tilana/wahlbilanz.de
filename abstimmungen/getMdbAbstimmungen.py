1b710c297ffa363d9d4492df8f471a2f08791319
tilana <nataliewidmann@gmx.de>
2017-09-16 13:13:25 +0200

    standardize names for parties and remove cities mentioned with the names

--- abstimmungen/getMdbAbstimmungen.py
+++ abstimmungen/getMdbAbstimmungen.py
@@ -16,9 +16,17 @@ def dropColumns(columns):
     return data.drop(columns, axis=1)
 
 
+def removeNameAddition(name):
+    startInd = name.find('(')
+    if startInd == -1:
+        return name
+    else:
+        return name[:startInd-1]
+
+
 def getMdbAbstimmungen():
-    
-    data = [] 
+
+    data = []
     abstimmungen = pd.read_csv('../../data/Abstimmungen.csv')
     for ind,row in abstimmungen.iterrows():
         link = row.csv
@@ -27,12 +35,16 @@ def getMdbAbstimmungen():
         abstimmung = pd.read_csv('../' + row.csv)
         setVotingCategory(abstimmung, title)
         abstimmung = abstimmung[COLS + [title]]
+        abstimmung.replace('DIE LINKE.', 'DIE LINKE', inplace=True)
+        abstimmung.replace('Fraktionslos', 'fraktionslos', inplace=True)
+        abstimmung.replace('BÜNDNIS`90/DIE GRÜNEN', 'BÜ90/GR', inplace=True)
+        abstimmung['Name'] = abstimmung.Name.apply(removeNameAddition)
         data.append(abstimmung)
 
 
-    mdbAbstimmungen = reduce(lambda df1, df2: df1.merge(df2, on=['Name', 'Vorname'], how='outer'), data)
+    mdbAbstimmungen = reduce(lambda df1, df2: df1.merge(df2, on=['Name', 'Vorname', 'Fraktion/Gruppe'], how='outer'), data)
     mdbAbstimmungen.rename(columns = {COLS[2]: 'party', 'Name':'name', 'Vorname':'first_name'}, inplace=True)
-    
+
     mdbAbstimmungen.to_csv('../../data/mdbAbstimmungen.csv')
 
 
